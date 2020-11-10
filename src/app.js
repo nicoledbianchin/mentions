2f1447d0d51f891598ea477c5c7ffc69330392e6
nicoledbianchin <nicoledbianchin@gmail.com>
2020-11-10 13:21:57 -0300

    :card_file_box: create database connection

--- src/app.js
+++ src/app.js
@@ -1,6 +1,33 @@
 const express = require('express')
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
+
+mongoose.connect(process.env.DATABASE_CONNECTION_STRING, {
+    useNewUrlParser: true
+});
+
+const database = mongoose.connection;
+
+database.on('connected', () => {
+    console.log('Mongoose default connection is open');
+});
+
+database.on('error', error => {
+    console.log(`Mongoose default connection has occured \n${error}`);
+});
+
+database.on('disconnected', () => {
+    console.log('Mongoose default connection is disconnected');
+});
+
+process.on('SIGINT', () => {
+    database.close(() => {
+        console.log('Mongoose default connection is disconnected due to application termination');
+        process.exit(0);
+    });
+})
 
 const indexRoutes = require('./routes/index-routes');
 app.use('/', indexRoutes);
